2edbfaad82e05759c347c5d766cda1aff0d34e2f
T.Suzaki <info@aurized-studio.jp>
2014-08-20 02:23:17 +0900

    #9 Topページ .product部分作成

--- htdocs/js/style.js
+++ htdocs/js/style.js
@@ -1,6 +1,31 @@
+ $(function(){
+
+heightline();
+productMenu();
+
+/**
+ * プロダクト ホバーでメニューを表示する
+ */
+function productMenu(){
+    var pi = $('.product-item');
+    pi.each(function(){
+        $(this).on({
+            'mouseenter': function(){
+                $(this).find('.product-item-desc-nav').css({'display': 'block'});
+            },
+            'mouseleave': function(){
+                $(this).find('.product-item-desc-nav').css({'display': 'none'});
+            }
+        });
+    });
+}
+
+
 /**
  * jquery.heightLine.js 要素の高さを揃える
  */
- $(function(){
+function heightline(){
     $(".addon-item").heightLine();
+}
+
  });
